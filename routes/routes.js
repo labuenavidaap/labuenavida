5c20e2ca7d33abce1b97e1f9bf8d55a041d082b3
miguelbachi5a <miguelbachi5a@gmail.com>
2020-09-07 17:56:35 +0200

    feat: product with best rate

--- routes/routes.js
+++ routes/routes.js
@@ -45,34 +45,7 @@ router.post('/product/:id/cart/delete', sessionMiddleware.authenticated, cartCon
 router.get('/confirm-order/:id', sessionMiddleware.authenticated, cartMiddleware.itemsInCart, cartController.renderConfirmOrder)
 router.post('/confirm-order/:id/pay', sessionMiddleware.authenticated, cartController.payment)
 router.get('/thank-you/:id', sessionMiddleware.authenticated, cartMiddleware.itemsInCart, cartController.renderThankYou)
-<<<<<<< HEAD
-
-router.post('/product/:id/wishlist', sessionMiddleware.authenticated,userController.addToWishList)
-router.post('/product/:id/wishlist/delete', sessionMiddleware.authenticated,userController.removeFromWishList)
-
-
-
-// USER ROUTES
-
-router.get('/login', sessionMiddleware.noAuthenticated, userController.renderLogin)
-router.post('/login', sessionMiddleware.noAuthenticated, userController.login)
-router.post('/logout', sessionMiddleware.authenticated, userController.logout)
-router.get('/auth/google', sessionMiddleware.noAuthenticated,userController.doSocialLoginGoogle)
-router.get('/auth/google/callback', sessionMiddleware.noAuthenticated, userController.googleCallback)
-// router.get('/user-from-google',sessionMiddleware.authenticated, userController.userFromGoogle)
-router.get('/user/signup', sessionMiddleware.noAuthenticated, userController.renderSignup)
-router.post('/signup', sessionMiddleware.noAuthenticated,/*fileUploader.single('avatar')*/ userController.create)
-router.get('/users/:id/activate/:token',sessionMiddleware.noAuthenticated, userController.activateUser)
-router.get('/users/:id', sessionMiddleware.authenticated, cartMiddleware.itemsInCart,userController.showProfile)
-router.get('/users/:id/edit', sessionMiddleware.authenticated, cartMiddleware.itemsInCart, userController.editUser)
-router.post('/users/:id/edit', sessionMiddleware.authenticated, cpUpload, userController.updateProfile)
-router.get('/become-producer/:id', sessionMiddleware.authenticated, userController.becomeProducer)
-router.post('/users/:id/delete', sessionMiddleware.authenticated, userController.delete)
-router.get('/producer-profile/:id', sessionMiddleware.authenticated, userController.renderPublicProfile)
-
-=======
 router.post('/product/:id/wishlist', sessionMiddleware.authenticated, cartController.addToWishList)
 router.post('/product/:id/wishlist/delete', sessionMiddleware.authenticated, cartController.removeFromWishList)
->>>>>>> master
 
 module.exports = router
